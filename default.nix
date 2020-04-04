4313b3da178a89756bffe59506cb3fe4d9b91df2
Chris S <chris@soundshare.co.uk>
2020-04-04 19:43:25 +0100

    Remove unused dependencies + fix niobium to point to github repo

--- default.nix
+++ default.nix
@@ -12,21 +12,14 @@ let
   pkgs = niv.pkgs;
   myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
-      # niobiumcoconut  = self.callCabal2nix "niobiumcoconut"
-      #   (builtins.fetchGit {
-      #     url = "https://github.com/chrissound/NiobiumCoconut.git";
-      #     rev = "7a9c50a3de74568ab5dd7d15d1f90da754a7e11a";
-      #   }) {};
       niobiumcoconut  = self.callCabal2nix "niobiumcoconut"
-        (/home/chris/fromLaptopt/usbflash/Haskell/NiobiumCoconut/.) {};
-      scotty-session  = (import (builtins.fetchTarball "https://github.com/chrissound/scotty-session/archive/0.0.6.tar.gz") {});
+        (builtins.fetchGit {
+          url = "https://github.com/chrissound/NiobiumCoconut.git";
+          rev = "7a9c50a3de74568ab5dd7d15d1f90da754a7e11a";
+        }) {};
     };
   };
 in
 (myHaskellPackages.callCabal2nix "blog3000" (./.) {}).overrideAttrs (oldAttrs: {
-  # postInstall = oldAttrs.postInstall or "" + ''
-  #   wrapProgram $out/bin/hello \
-  #     --add-flags "-t"
-  # '';
   buildInputs = oldAttrs.buildInputs or [] ++ [ pkgs.imagemagick ];
 })
