e944f59c37a8ebc469fa063e99cee37e01db2c12
Chris S <chris@soundshare.co.uk>
2020-09-02 17:05:23 +0100

    fix nix dependencies

--- default.nix
+++ default.nix
@@ -13,15 +13,15 @@ let
   pkgs = niv.pkgs;
   myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
-      niobiumcoconut  = self.callCabal2nix "niobiumcoconut"
+      niobiumcoconut  = import
         (builtins.fetchGit {
           url = "https://github.com/chrissound/NiobiumCoconut.git";
           rev = "18886b6276e61d62b9bfb5cfb71b8892c9fd2d30";
         }) {};
       #niobiumcoconut-lucid  = import /home/chris/NewProjects/NiobiumCoconut-Lucid/. {};
-      niobiumcoconut-lucid  = self.callCabal2nix "niobiumcoconut-lucid"
+      niobiumcoconut-lucid  = import
         (builtins.fetchGit {
-          url = "https://github.com/chrissound/NiobiumCoconut.git";
+          url = "https://github.com/chrissound/NiobiumCoconut-Lucid.git";
           rev = "d7684ce933005c24cc867acdc30185aaff5da26a";
         }) {};
         #(/home/chris/NewProjects/NiobiumCoconut-Lucid/.) {};
