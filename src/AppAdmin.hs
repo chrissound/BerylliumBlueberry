chrissound/BerylliumBlueberry

commit bbba40259f18f70e560f710aaa3803f0bd9f35ef

Addition of tagged posts

src/AppAdmin.hs
@@ -96,11 +96,31 @@ loginAction' = do
           redirect $ cs $ R.renderPublicUrl R.Dashboard
         | otherwise -> renderPage t (panelWithErrorView t (Just "Failed to login") $ loginFormLucid' loginForm')
 
+select2sv :: Text -> MaybeNotification -> AppAction SiteView
+select2sv t v = do
+  sv <- svd t v
+  pure $ sv
+    { scripts =
+           [ ( "//code.jquery.com/jquery-2.2.4.min.js"
+             , Just "sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44"
+             , Just "anonymous"
+             )
+           , ( "//cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"
+             , Nothing
+             , Nothing
+             )
+           , ("/static/postform.js", Nothing, Nothing)
+           ]
+    , css     =
+        [ "//cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css"
+           ]
+    }
 
 viewCreatePostAction :: AppAction ()
 viewCreatePostAction = do
   let t = "Create Post"
-  renderPage' t Nothing (panelWithErrorView t Nothing  $ Forms.Post2.postFormLucid (Forms.Post2.postForm))
+  sv <- select2sv t Nothing
+  renderScottyHtmlSv sv (panelWithErrorView t Nothing  $ Forms.Post2.pageFormLucid (Forms.Post2.postForm))
 
 createPostAction :: AppAction ()
 createPostAction = do
@@ -136,27 +156,25 @@ createPostAction = do
 
 editPostAction :: Int -> AppAction ()
 editPostAction x = do
-  let panel m v = panelWithErrorView "Edit Post" (m) $ Forms.Post2.postEditFormLucid x v
+  let t = "Edit Post"
+  let panel m v = panelWithErrorView t (m) $ Forms.Post2.postEditFormLucid x v
   formInput <- scottyFormInput
-  let f = runInputForm Forms.Post2.postForm inputPost $ formInput
-  case f of
-    Right p -> AppAdmin.processPost p ((flip panel) Forms.Post2.postForm)
+  case runInputForm Forms.Post2.postForm inputPost $ formInput of
+    Right p -> do
+      AppAdmin.processPost p (flip panel $ Forms.Post2.postForm)
     Left nferr -> do
+      liftIO $ pPrint nferr
       let extra = panel Nothing $ nferr
-      renderPage' ("Edit Post") (Just ("Error submitting comment", NotificationError)) (extra)
+      sv <- select2sv t Nothing
+      renderScottyHtmlSv sv (panelWithErrorView t (Just "Error submitting comment") extra)
 
 editPostAction' :: Int -> AppAction ()
 editPostAction' x = do
+  let t = "Edit Post"
   p <- getPostElseError x
-  renderPage'
-    "Edit Post"
-    Nothing
-    (
-      panelWithErrorView "Edit Post" Nothing
-      $ Forms.Post2.postEditFormLucid
-      (idInteger $ Post.postId p)
-      (postForm' p)
-    )
+  sv <- select2sv t Nothing
+  let c = Forms.Post2.postEditFormLucid (idInteger $ Post.postId p) (postForm' p)
+  renderScottyHtmlSv sv (panelWithErrorView t Nothing c)
 
 deletePostAction :: Int -> AppAction ()
 deletePostAction x = do

default.nix
@@ -18,6 +18,13 @@ let
           url = "https://github.com/chrissound/NiobiumCoconut.git";
           rev = "18886b6276e61d62b9bfb5cfb71b8892c9fd2d30";
         }) {};
+      #niobiumcoconut-lucid  = import /home/chris/NewProjects/NiobiumCoconut-Lucid/. {};
+      niobiumcoconut-lucid  = self.callCabal2nix "niobiumcoconut-lucid"
+        (builtins.fetchGit {
+          url = "https://github.com/chrissound/NiobiumCoconut.git";
+          rev = "d7684ce933005c24cc867acdc30185aaff5da26a";
+        }) {};
+        #(/home/chris/NewProjects/NiobiumCoconut-Lucid/.) {};
     };
   };
 in

src/AppAdminSinglePage.hs
@@ -37,7 +37,25 @@ adminSinglePage = do
   get (webRoute R.AdminCreateSinglePage) $ do
     verifyAuth
     let t = "Create Page"
-    renderPage' t Nothing (panelWithErrorView t Nothing  $ Forms.Post2.pageFormLucid (Forms.Post2.postForm))
+    sv <- (svd t Nothing)
+    let sv' =
+          sv
+            { scripts =
+                   [ ( "//code.jquery.com/jquery-2.2.4.min.js"
+                     , Just "sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44"
+                     , Just "anonymous"
+                     )
+                   , ( "//cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"
+                     , Nothing
+                     , Nothing
+                     )
+                   , ("/static/postform.js", Nothing, Nothing)
+                   ]
+            , css     =
+                [ "//cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css"
+                   ]
+            }
+    renderScottyHtmlSv sv' (panelWithErrorView t Nothing  $ Forms.Post2.pageFormLucid (Forms.Post2.postForm))
   post (webRoute R.AdminCreateSinglePage) $ do
     verifyAuth
     createPageAction

src/AppPost.hs
@@ -18,6 +18,9 @@ import Template.Post
 import Forms.Comment
 import Models.Associations
 import Lucid
+import Data.Text (Text)
+import Data.List
+import Network.HTTP.Types
 
 getPostElseError :: Int -> AppAction Post.Post
 getPostElseError x = do
@@ -34,13 +37,28 @@ postsViewPage = do
   (postHook, preHook) <- AppCommon.session >>= \case
     Just _ -> return (postViewExtraAdmin, postsViewExtraAdmin)
     Nothing -> return (postViewExtraEmpty, return ())
-  withSvRenderPage "Blog" (\sv -> preHook <> postsView sv (postV <$> posts) postHook)
+  withSvRenderPage "Blog" (\sv -> preHook <> postsView sv (postV <$> posts) postHook "Blog")
+
+postsViewPageTag :: Text -> AppAction ()
+postsViewPageTag x = do
+  posts <- fmap (filter (elem x . getPostTags . postTags . postV)) $ liftAndCatchIO $ getPagePosts' (PostTypeBlog)
+  case posts of
+    [] -> status status404
+    _ -> do
+      (postHook, preHook) <- AppCommon.session >>= \case
+        Just _ -> return (postViewExtraAdmin, postsViewExtraAdmin)
+        Nothing -> return (postViewExtraEmpty, return ())
+      let t = mconcat $ intersperse " " ["Blog","-",x]
+      withSvRenderPage t (\sv -> preHook <> postsView sv (postV <$> posts) postHook (cs t))
 
 
 listPost :: AppServer ()
 listPost = do
   get (webRoute R.ListPost)$ do
     postsViewPage
+  get (webRoute $ R.TaggedPost "tag") $ do
+    t <- param "tag"
+    postsViewPageTag t
   get (webRoute $ R.ViewPost "postId")$ do
     c <- liftAndCatchIO connection
     (param "postId" >>= return . filterEid)

src/AppRender.hs
@@ -33,10 +33,8 @@ withSvRenderPage' :: Text -> MaybeNotification -> (SiteView -> Html ()) -> AppAc
 withSvRenderPage' t n h = siteViewData' t n
   >>= (\sv -> renderScottyHtmlSv sv (h sv))
 
-
 renderScottyHtmlSv :: (ScottyError e, MonadIO m) => SiteView -> Html () -> ActionT e m ()
-renderScottyHtmlSv sv = do
-  html . cs . renderText . (siteView sv)
+renderScottyHtmlSv sv = html . cs . renderText . (siteView sv)
 
 formAction ::
   NioForm
@@ -67,6 +65,8 @@ svd t n = do
       (siteContactEmail appConfig)
       (siteSideBarHtml appConfig)
       (AppConfig.siteExtraHeadHtml appConfig)
+      []
+      []
     ) <$> getLoggedInUser <*> liftIO getPagePosts <*> liftIO getAppConfig <*> (liftAndCatchIO $ findAll c)
 
 siteViewDataDef :: Text -> AppAction SiteView

src/Forms/Post2.hs
@@ -8,53 +8,41 @@ import NioFormTypes
 import Routes as R
 import Common
 import Control.Monad
-
---import Control.Monad
-
 import Lucid
 import Forms.Forms2
 import MyNioFormHtml
-
---import DigestiveFunctorsPostgreSQL
---import Forms.Forms
 import Models.Post as MdlP
--- import Models.Comment as MdlC
--- import NioFormExtra
---import qualified Routes as R
 
 postForm :: NioForm
-postForm =
-  NioForm [
-       NioFieldView "" "postId" emptyError
-         NioFieldInputHidden (NioFieldValS "")
-     , NioFieldView "postTitle" "postTitle" emptyError
-         NioFieldInputTextShort (NioFieldValS "")
-     , NioFieldView "postEasyId" "postEasyId" emptyError
-         NioFieldInputTextShort (NioFieldValS "")
-     , NioFieldView "postBody" "postBody" emptyError
-         NioFieldInputText (NioFieldValS "")
-     , NioFieldView "postCreated" "postCreated" emptyError
-         NioFieldInputTextShort (NioFieldValS "")
-  ]
+postForm = postForm'' Nothing
 
 postForm' :: Post -> NioForm
-postForm' p =
+postForm' p = postForm'' $ Just p
+
+postForm'' :: Maybe Post -> NioForm 
+postForm'' mp =
   NioForm [
        NioFieldView "" "postId" emptyError
-         NioFieldInputHidden (NioFieldValS $ show $ idInteger $ postId p)
+         NioFieldInputHidden $ maybe (NioFieldValS "") (NioFieldValS . show . idInteger . postId) mp
      , NioFieldView "postTitle" "postTitle" emptyError
-         NioFieldInputTextShort (NioFieldValS $ cs $ postTitle p)
+         NioFieldInputTextShort $ maybe (NioFieldValS "") (NioFieldValS . cs . postTitle) mp
      , NioFieldView "postEasyId" "postEasyId" emptyError
-         NioFieldInputTextShort (NioFieldValS $ cs $ postEasyId p)
+         NioFieldInputTextShort $ maybe (NioFieldValS "") (NioFieldValS . cs . postEasyId) mp
      , NioFieldView "postBody" "postBody" emptyError
-         NioFieldInputText (NioFieldValS $ cs $ postBody p)
+         NioFieldInputText $ maybe (NioFieldValS "") (NioFieldValS . cs . postBody) mp
+     , NioFieldView "postTags" "postTags" emptyError
+         -- NioFieldInputText $ maybe (NioFieldValS "") (NioFieldValS . cs . postBody) mp
+
+        (NioFieldInputLabled True [("Test","hmm"), ("Test123","123hmm")])
+        (NioFieldValM $ maybe [] (const []) mp)
      , NioFieldView "postCreated" "postCreated" emptyError
-         NioFieldInputTextShort (NioFieldValS $ show $ postCreated p)
+         NioFieldInputTextShort $ maybe (NioFieldValS "") (NioFieldValS . show . postCreated) mp
+
   ]
 
 inputPost :: FormInput -> Either ([FieldEr]) Post
 inputPost fi = do
-  (((liftM5 Post) <$> a <*> b <*> c <*> d <*> e) >>= \case
+  (((liftM6 Post) <$> a <*> b <*> c <*> d <*> e <*> f) >>= \case
     Right x' -> pure $ pure x'
     Left _ -> (\_ -> do
                   Left allErrors
@@ -66,6 +54,7 @@ inputPost fi = do
                      , getFormErrors fi [c]
                      , getFormErrors fi [d]
                      , getFormErrors fi [e]
+                     , getFormErrors fi [f]
                      ]
       a = fieldValue isPresent "postId"
       b = fieldValue isPresent "postTitle"
@@ -76,6 +65,7 @@ inputPost fi = do
       e = fieldValue (allRules [
                          minLength 3
                          ]) "postEasyId"
+      f = fieldValue always' "postTags"
 
 postFormLucid :: NioForm -> Html ()
 postFormLucid nf = nioformHtml $ NioFormHtml nf (R.AdminCreatePost)

src/Main.hs
@@ -128,6 +128,10 @@ up6 c = do
       ]
     ) c
 
+up7 :: Connection -> IO ()
+up7 c = do
+  migrate (add_column "post" "tags" "jsonb") c
+
 dropAll :: IO ()
 dropAll = do
   c <- connection
@@ -180,6 +184,7 @@ main = do
     ("--migrationsUp":"4":[]) -> connection >>= up4 >> exitWith ExitSuccess
     ("--migrationsUp":"5":[]) -> connection >>= up5 >> exitWith ExitSuccess
     ("--migrationsUp":"6":[]) -> connection >>= up6 >> exitWith ExitSuccess
+    ("--migrationsUp":"7":[]) -> connection >>= up7 >> exitWith ExitSuccess
     ("--migrate":[]) -> migrateAll >> exitWith ExitSuccess
     ("--dropAll":[]) -> dropAll >> exitWith ExitSuccess
     ("--init":[]) -> initConfig

src/Models/Post.hs
@@ -11,8 +11,12 @@ import Data.Text
 import GHC.Generics
 import Data.String.Conversions
 import Data.String
-
+import           Database.PostgreSQL.Simple.FromField
+import           Database.PostgreSQL.Simple.ToField
+import Data.ByteString.Lazy
 import Common
+import Data.Aeson
+import Data.Aeson.Types
 
 data Post = Post {
     postId :: DBKey
@@ -20,7 +24,21 @@ data Post = Post {
   , postBody :: Text
   , postCreated :: UTCTimestamp
   , postEasyId :: Text
+  , postTags :: PostTags
   } deriving (Generic, Show)
+
+data PostTags = PostTags [Text] deriving Show
+
+getPostTags :: PostTags -> [Text]
+getPostTags (PostTags x) = x
+
+instance FromField PostTags where
+   fromField f x = do
+     parseMaybe parseJSON <$> fromField f x >>= \case
+       Just m' -> pure $ PostTags m'
+       Nothing -> returnError Incompatible f "PostTags ???"
+instance ToField PostTags where
+  toField (PostTags x) = toField $ encode x
 
 instance Model Post
 

commit 404e41dbe050d7453bfed7e334c03f0f47ab5827

Fix post creation form to use correct form renderer
The viewCreatePostAction was incorrectly calling Forms.Post2.pageFormLucid
instead of Forms.Post2.postFormLucid, causing new posts created from
/admin/post/create/ to submit to the page creation endpoint instead of
the post creation endpoint.

This bug was introduced in commit bbba402 during the "Addition of tagged posts"
refactoring when switching to use select2sv.

src/AppAdmin.hs
@@ -120,7 +120,7 @@ viewCreatePostAction :: AppAction ()
 viewCreatePostAction = do
   let t = "Create Post"
   sv <- select2sv t Nothing
-  renderScottyHtmlSv sv (panelWithErrorView t Nothing  $ Forms.Post2.pageFormLucid (Forms.Post2.postForm))
+  renderScottyHtmlSv sv (panelWithErrorView t Nothing  $ Forms.Post2.postFormLucid (Forms.Post2.postForm))
 
 createPostAction :: AppAction ()
 createPostAction = do