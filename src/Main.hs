4b6694cadaa3e7a3cef588ea92cfbf5d75226f8b
Chris S <chris@soundshare.co.uk>
2025-11-10 15:40:31 +0000

    remove migration unused code

--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,6 @@
 module Main where
 
 import Database.PostgreSQL.Simple
--- import Database.PostgreSQL.ORM
--- import Database.PostgreSQL.Migrations
--- import Database.PostgreSQL.ORM.Model
-
 import Control.Exception
 import Models.Comment as Comment
 import Models.Post as Post
@@ -33,139 +29,11 @@ import App
 import Database
 import AppConfig
 
--- Removed ORM-specific code: commentPostRefinfo, migrateAll
--- Migrations should now be handled using a different migration tool or raw SQL scripts
--- commentPostRefinfo :: DBRefInfo Comment Post
--- commentPostRefinfo = defaultDBRefInfo
-
-migrateAll :: IO ()
-migrateAll = do
-  putStrLn "Migrations have been disabled - please use a migration tool like dbmate or run SQL scripts directly"
-  pure ()
-
-{- Original migration code commented out - replace with proper migration tool
--- up c
--- up2 c
--- up3 c
--- up4 c
--- up5 c
--- up6 c
--}
-
-{- All migration functions commented out - use a proper migration tool instead
-
-up :: Connection -> IO ()
-up c = do
-  migrate ( do
-    create_table "post"
-      [ column "postId" "serial PRIMARY KEY"
-      , column "postTitle" "text NOT NULL"
-      , column "postBody" "text NOT NULL"
-      , column "postCreated" "timestamptz NOT NULL"
-      , column "postEasyId" "text NOT NULL"
-      ]
-    ) c
-  migrate ( do
-    create_table "comment"
-      [ column "commentId" "serial PRIMARY KEY"
-      , column "postId" "integer references post"
-      , column "commentBody" "text NOT NULL"]
-    ) c
-  migrate ( do
-    create_table "user"
-      [ column "userId" "serial PRIMARY KEY"
-      , column "userName" "text NOT NULL"
-      , column "userEmail" "text NOT NULL"
-      , column "userPassword" "text NOT NULL"
-      , column "userVerified" "boolean"]
-    ) c
-
-up2 :: Connection -> IO ()
-up2 c = do
-  migrate (add_column "comment" "approved" "boolean") c
-  migrate (add_column "comment" "postCreated" "timestamptz NOT NULL") c
-  migrate (add_column "comment" "authorAlias" "text") c
-
-up3 :: Connection -> IO ()
-up3 c = do
-  migrate ( do
-    create_table "postType"
-      [ column "postTypeId" "serial PRIMARY KEY"
-      , column "postId" "integer references post"
-      , column "postType" "integer NOT NULL"
-      ]
-    ) c
-  z <- dbSelect c (modelDBSelect :: DBSelect Post)
-  mapM_ (\p -> do
-            s <- trySave c (PostType (NullKey) (mkDBRef p) (fromEnum $ PostTypeBlog))
-            case s of
-              Right _ -> pure ()
-              Left x -> error $ show x
-           ) z
-
-up4 :: Connection -> IO ()
-up4 c = do
-  migrate ( do
-    create_table "image"
-      [ column "imageId" "serial PRIMARY KEY"
-      , column "imageTitle" "text NOT NULL"
-      , column "imageEasyId" "text NOT NULL"
-      , column "imageCreated" "timestamptz NOT NULL"
-      , column "imageFile" "text NOT NULL"
-      ]
-    ) c
-
-up5 :: Connection -> IO ()
-up5 _ = do
-  pure ()
-
-up6 :: Connection -> IO ()
-up6 c = do
-  migrate ( do
-    create_table "file"
-      [ column "fileId" "serial PRIMARY KEY"
-      , column "fileTitle" "text NOT NULL"
-      , column "fileEasyId" "text NOT NULL"
-      , column "fileCreated" "timestamptz NOT NULL"
-      , column "fileFile" "text NOT NULL"
-      ]
-    ) c
-
-up7 :: Connection -> IO ()
-up7 c = do
-  migrate (add_column "post" "postTags" "jsonb") c
-
-dropAll :: IO ()
-dropAll = do
-  c <- connection
-  _ <- dropTableCascade "post" c
-  _ <- dropTableCascade "comment" c
-  _ <- dropTableCascade "\"user\"" c
-  pure ()
-
-executeQuery_ :: Query -> Migration Int64
-executeQuery_ q = ask >>= \conn -> liftIO $ execute_ conn q
-
-dropTableCascade :: String  -> Connection -> IO Int64
-dropTableCascade x c = do
-  let z = mconcat ( D.List.intersperse " " ["drop table", x, "cascade"] :: [String])
-  print z
-  execute_ c (fromString $ z)
-
--}
-
-data ManyToOne a b = ManyToOne a [b]
-
 -- | Run @action@ every @period@ seconds.
 runPeriodicallyBigDrift :: Double -> IO () -> IO ()
 runPeriodicallyBigDrift period action = forever $ do
   action
   threadDelay (round $ period * 10 ** 6)
-
-migrationsUp :: IO ()
-migrationsUp = do
-  putStrLn "Migrations have been disabled - please use a migration tool like dbmate or run SQL scripts directly"
-  pure ()
 
 appState :: String
 appState = "data/appstate.bin"
@@ -184,15 +52,6 @@ main = do
   args <- getArgs
   case args of
     [] -> pure ()
-    ("--migrationsUp":[]) -> migrationsUp >> exitWith ExitSuccess
-    ("--migrationsUp":"2":[]) -> migrationsUp >> exitWith ExitSuccess
-    ("--migrationsUp":"3":[]) -> migrationsUp >> exitWith ExitSuccess
-    ("--migrationsUp":"4":[]) -> migrationsUp >> exitWith ExitSuccess
-    ("--migrationsUp":"5":[]) -> migrationsUp >> exitWith ExitSuccess
-    ("--migrationsUp":"6":[]) -> migrationsUp >> exitWith ExitSuccess
-    ("--migrationsUp":"7":[]) -> migrationsUp >> exitWith ExitSuccess
-    ("--migrate":[]) -> migrateAll >> exitWith ExitSuccess
-    ("--dropAll":[]) -> putStrLn "dropAll has been disabled - please use a migration tool" >> exitWith ExitSuccess
     ("--init":[]) -> initConfig
     (_) -> print "Possibly unknown args"
   let port = case args of
